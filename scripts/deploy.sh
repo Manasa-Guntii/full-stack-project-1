8cd10f82d4dd775a7b18f0f9e85adc9dee2e7003
vikat <vikat@vikats-MacBook-Pro.local>
2026-05-09 18:56:53 +0530

    fix final CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Stopping existing application"
-pkill java || true
+echo "Stopping old app safely"
+pkill -f demo-0.0.1-SNAPSHOT.jar || true
 
-echo "Starting new application"
+echo "Starting new app"
+
 cd /home/ec2-user/app
 
 nohup java -jar demo-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
