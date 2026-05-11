4db7da726dfad3fbc0984c473bdcd67485504a77
vikat <vikat@vikats-MacBook-Pro.local>
2026-05-11 12:51:03 +0530

    final cicd setup completed

--- demo/src/main/java/com/manasa/demo/controller/UserController.java
+++ demo/src/main/java/com/manasa/demo/controller/UserController.java
@@ -2,9 +2,7 @@ package com.manasa.demo.controller;
 
 import com.manasa.demo.model.User;
 import com.manasa.demo.repository.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,41 +15,28 @@ public class UserController {
     @Autowired
     private UserRepository repo;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    // SIGNUP
     @PostMapping("/signup")
     public User signup(@RequestBody User user) {
-
-        // HASH PASSWORD BEFORE SAVING
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
         return repo.save(user);
     }
 
-    // LOGIN
     @PostMapping("/login")
     public User login(@RequestBody User user) {
 
         User db = repo.findByEmail(user.getEmail());
 
-        if (db != null &&
-                passwordEncoder.matches(user.getPassword(), db.getPassword())) {
-
+        if (db != null && db.getPassword().equals(user.getPassword())) {
             return db;
         }
 
         return null;
     }
 
-    // GET ALL USERS
     @GetMapping("/users")
     public List<User> getAll() {
         return repo.findAll();
     }
 
-    // UPDATE USER
     @PutMapping("/update/{id}")
     public User update(@PathVariable String id, @RequestBody User user) {
 
@@ -61,11 +46,7 @@ public class UserController {
 
             existing.setName(user.getName());
             existing.setEmail(user.getEmail());
-
-            // HASH UPDATED PASSWORD
-            existing.setPassword(
-                    passwordEncoder.encode(user.getPassword())
-            );
+            existing.setPassword(user.getPassword());
 
             return repo.save(existing);
         }
@@ -73,7 +54,6 @@ public class UserController {
         return null;
     }
 
-    // DELETE USER
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable String id) {
 
